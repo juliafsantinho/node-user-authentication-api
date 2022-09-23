a79b39407567c3ed08c51234f1c272c324b379ad
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-09-23 11:34:19 -0300

    feat(routes): starting user CRUD development

--- MS-AUT/src/index.ts
+++ MS-AUT/src/index.ts
@@ -1,10 +1,13 @@
 import express, {Request, Response, NextFunction} from 'express';//biblioteca
+import usersRoute from './routes/users.route';
 
 const app = express();//instancia da biblioteca
 
 //configurando as rotas
+app.use(usersRoute);
+
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Sucesso TOTAL! ' });
+    res.status(200).send({ foo: 'Sucesso! ' });
 });
 
 app.listen(3000, () => {
