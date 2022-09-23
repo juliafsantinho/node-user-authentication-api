7876acaaceb38a7f3733071315cf9c76deba1d48
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-09-23 14:27:17 -0300

    feat(routes-status): refactoring the status route

--- MS-AUT/src/index.ts
+++ MS-AUT/src/index.ts
@@ -1,18 +1,16 @@
-import express, {Request, Response, NextFunction} from 'express';//biblioteca
+import express from 'express'; //biblioteca
+import statusRoute from './routes/status.route';
 import usersRoute from './routes/users.route';
 
 const app = express();//instancia da biblioteca
 
 //Configurando a Aplicação
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 //Configurações de Rotas
 app.use(usersRoute);
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Sucesso! ' });
-});
+app.use(statusRoute);
 
 //Inicialização do Servidor
 app.listen(3000, () => {
