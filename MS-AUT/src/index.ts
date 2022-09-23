7004e49a3d02e40ac0d86017a255610f8bdd7e12
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-09-23 10:55:38 -0300

    feat(index.ts): server creation with express

--- MS-AUT/src/index.ts
+++ MS-AUT/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Executei!')
+import express, {Request, Response, NextFunction} from 'express';//biblioteca
+
+const app = express();//instancia da biblioteca
+
+//configurando as rotas
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'bar' });
+
+});
+
+app.listen(3000, () => {
+    console.log('App executando a porta 3000.')
+})
