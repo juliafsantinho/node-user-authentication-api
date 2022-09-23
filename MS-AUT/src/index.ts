ad66e471a8678bbf8ed5143cb9d895c71f36339b
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-09-23 12:16:31 -0300

    feat(routes): developing the post method

--- MS-AUT/src/index.ts
+++ MS-AUT/src/index.ts
@@ -3,13 +3,18 @@ import usersRoute from './routes/users.route';
 
 const app = express();//instancia da biblioteca
 
-//configurando as rotas
+//Configurando a Aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Configurações de Rotas
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({ foo: 'Sucesso! ' });
 });
 
+//Inicialização do Servidor
 app.listen(3000, () => {
     console.log('App executando a porta 3000.')
 })
--- MS-AUT/src/routes/users.route.ts
+++ MS-AUT/src/routes/users.route.ts
@@ -7,14 +7,18 @@ const usersRoute = Router();
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) =>{
     const users = [{ userName: 'julia'}];
     res.status(StatusCodes.OK).send(users);
-
 });
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid:string }>, res: Response, next: NextFunction) =>{
     const uuid = req.params.uuid;
  // bancoDeDados.getUserByUuid(uuid);
     res.status(StatusCodes.OK).send({ uuid });
+});
 
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+    console.log(req.body);
+    res.status(StatusCodes.CREATED).send(newUser);
 });
 
 export default usersRoute;
