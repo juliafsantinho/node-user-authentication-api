9304096fbe5f6775d7f56159d1612789e3d95a6f
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-09-23 11:14:53 -0300

    feat(index.ts): automating the server to identify changes

--- MS-AUT/src/index.ts
+++ MS-AUT/src/index.ts
@@ -4,8 +4,7 @@ const app = express();//instancia da biblioteca
 
 //configurando as rotas
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'bar' });
-
+    res.status(200).send({ foo: 'Sucesso TOTAL! ' });
 });
 
 app.listen(3000, () => {
