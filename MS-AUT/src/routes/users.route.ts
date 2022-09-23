486c0c66b8365a0713a7bf421927f95fe055d733
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-09-23 11:58:53 -0300

    feat(routes): implementing statusCodes in code

--- MS-AUT/src/routes/users.route.ts
+++ MS-AUT/src/routes/users.route.ts
@@ -1,17 +1,19 @@
+
 import { Router, Request, Response, NextFunction } from "express";
+import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) =>{
     const users = [{ userName: 'julia'}];
-    res.status(200).send(users);
+    res.status(StatusCodes.OK).send(users);
 
 });
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid:string }>, res: Response, next: NextFunction) =>{
     const uuid = req.params.uuid;
  // bancoDeDados.getUserByUuid(uuid);
-    res.status(200).send({ uuid });
+    res.status(StatusCodes.OK).send({ uuid });
 
 });
 
