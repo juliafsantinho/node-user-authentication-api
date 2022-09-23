c661515ce9cd27ab94c632b46013450a9b54942f
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-09-23 14:15:53 -0300

    feat(routes): developing the put and delete methods

--- MS-AUT/src/index.ts
+++ MS-AUT/src/index.ts
@@ -5,7 +5,7 @@ const app = express();//instancia da biblioteca
 
 //Configurando a Aplicação
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); 
 
 //Configurações de Rotas
 app.use(usersRoute);
--- MS-AUT/src/routes/users.route.ts
+++ MS-AUT/src/routes/users.route.ts
@@ -21,4 +21,17 @@ usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
     res.status(StatusCodes.CREATED).send(newUser);
 });
 
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+
+    modifiedUser.uuid = uuid;
+
+    res.status(StatusCodes.OK).send(modifiedUser);
+});
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
+});
+
 export default usersRoute;
